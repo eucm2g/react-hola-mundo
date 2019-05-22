e6a1f7eb97a2c4624e615d2f504863930acb745d
Eugenio <infoprogramadornovatocom>
2019-05-22 01:54:42 -0500

    04 clases en reactjs

---
+++ src/MiClase/MiClase.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Miclase extends Component {
+    render (){
+        return (
+            <div className="contenido">
+                <h2>Titulo</h2>
+                <p>
+                    Contenido
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Miclase;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import MiApp from './MiApp/MiApp';
+import MiClase from './MiClase/MiClase';
 import * as serviceWorker from './serviceWorker';
 import array from './modelo/datos'
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<MiApp variable="texto" array={array} />, document.getElementById('MiElemento'));
-
+// ReactDOM.render(<MiApp variable="texto" array={array} />, document.getElementById('MiElemento'));
+ReactDOM.render(<MiClase />, document.getElementById('MiClase'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
