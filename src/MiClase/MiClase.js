e845cc4b27329efd2cdfd6bd69105991604b270a
Eugenio <infoprogramadornovatocom>
2019-05-22 03:27:36 -0500

    05 reactjs y state

--- src/MiClase/MiClase.js
+++ src/MiClase/MiClase.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 
 class Miclase extends Component {
+    constructor(){
+        super();
+        this.state={
+            count:1
+        };
+        this.clickBotones=this.clickBotones.bind(this);
+    }
+
+    componentDidMount(){
+        this.setState({
+            count:1
+        });
+    }
+
+    clickBotones(e){
+        if(e.target.id==="sumar"){
+            this.setState({
+                count:this.state.count+1
+            });
+        }
+        else if(e.target.id==="restar" && this.state.count > 1 ){
+            this.setState({
+                count:this.state.count-1
+            });
+        }
+        else if(e.target.id==="reiniciar"){
+            this.setState({
+                count:1
+            });
+        }
+    }
     render (){
         return (
             <div className="contenido">
-                <h2>Titulo</h2>
+                <h2>Contador {this.state.count}</h2>
                 <p>
-                    Contenido
+                <button id="sumar" onClick={this.clickBotones}>+</button>
+                <button id="restar" onClick={this.clickBotones}>-</button>
+                <button id="reiniciar" onClick={this.clickBotones}>Reiniciar</button>
                 </p>
             </div>
         );
